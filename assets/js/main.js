ce6bdfb6be6236dddf252101855690ce62fbc0da
xhimgir <himani@shinedezign.com>
2026-02-04 15:29:22 +0530

    worked on color scheme

--- assets/js/main.js
+++ assets/js/main.js
@@ -723,7 +723,7 @@ const initialiseAosAndTocbot = () => {
         tocbot.init({
             tocSelector: '.toc',
             contentSelector: '.page-content',  // 🔥 FIX IS HERE
-            headingSelector: 'h2',
+            headingSelector: 'h1, h2, h3',
             collapseDepth: 0
         });
     }
